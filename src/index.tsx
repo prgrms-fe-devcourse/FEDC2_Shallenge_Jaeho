1b02566cf00b8368cd1721dab19eab9f85071448
chogyejin <whrpwls96@naver.com>
2022-06-12 16:32:17 +0900

    chore: chakra ui 설정 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -7,6 +8,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
