6de24ebf9cc2d59ce367e5e2e8c25b3a7b314687
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-06-14 22:54:58 +0900

    feat: Footer 레이아웃 추가

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Icon from "@base/Icon";
+import ChakraText from "@base/ChakraText";
+
+type footerTypes = {
+  width: number;
+  height: number;
+};
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 640px;
+  height: 96px;
+`;
+
+const SubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Footer = ({ width = 640, height = 96 }: footerTypes) => {
+  const FooterStyle: React.CSSProperties = {
+    width,
+    height,
+  };
+
+  const footerList = [
+    {
+      name: "home",
+      nameKor: "홈",
+    },
+    {
+      name: "compass",
+      nameKor: "채널",
+    },
+    {
+      name: "bell",
+      nameKor: "알림",
+    },
+    {
+      name: "user",
+      nameKor: "로그인",
+    },
+  ];
+
+  return (
+    <Container style={FooterStyle}>
+      {footerList.map((item) => (
+        <SubContainer>
+          <Icon name={item.name} />
+          <ChakraText size="md" align="center">
+            {item.nameKor}
+          </ChakraText>
+        </SubContainer>
+      ))}
+    </Container>
+  );
+};
+
+export default Footer;
