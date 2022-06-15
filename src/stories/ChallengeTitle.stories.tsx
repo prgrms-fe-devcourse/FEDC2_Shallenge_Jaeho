d8845129ea2b181f74b01cbbc6871f18a64de122
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-06-15 15:08:14 +0900

    feat: ChallengeTitle 컴포넌트 추가

---
+++ src/stories/ChallengeTitle.stories.tsx
@@ -0,0 +1,20 @@
+import ChallengeTitle from "@domain/ChallengePage/ChallengeTitle";
+
+export default {
+  title: "Domain/ChallengeTitle",
+  component: ChallengeTitle,
+  argTypes: {
+    startDate: { defaultValue: "2022.05.23", control: "text" },
+    endDate: { defaultValue: "2022.06.23", control: "text" },
+    width: {
+      defaultValue: 610,
+      control: { type: "range", min: 360, max: 610 },
+    },
+    title: { defaultValue: "가지고 싶던 운동화 사기", control: "text" },
+    restDay: { defaultValue: 5, control: "number" },
+  },
+};
+
+export const Default = (args: any) => {
+  return <ChallengeTitle {...args} />;
+};
