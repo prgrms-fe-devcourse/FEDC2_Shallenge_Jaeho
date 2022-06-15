9d0460b1065261747ba940c93bd284515368800d
joSeugHee <seunghee.jo@8percent.kr>
2022-06-15 22:18:13 +0900

    feat: API 요청함수 정의

---
+++ src/api/post.ts
@@ -0,0 +1,28 @@
+import axios from "@lib/axios";
+
+export const fetchGetPostListByChannel = (channelId: string) =>
+  axios.get(`/posts/channel/${channelId}`);
+
+export const fetchGetUserPostList = (authorId: string) =>
+  axios.get(`/posts/author/${authorId}`);
+
+export const fetchPostPostByChannelId = ({
+  title,
+  image,
+  channelId,
+}: {
+  title: string;
+  image?: string;
+  channelId: string;
+}) =>
+  axios.post("/posts/create", {
+    data: { title, image, channelId },
+  });
+
+export const fetchGetPostByPostId = (postId: string) =>
+  axios.get(`/posts/${postId}`);
+
+export const fetchDeletePostByPostId = (id: string) =>
+  axios.delete(`/posts/delete`, {
+    data: { id },
+  });
