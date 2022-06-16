c9db3f6f25146ba2c6f51e41e35019534163d912
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-06-17 02:54:17 +0900

    feat: CommentButton 컴포넌트 추가

---
+++ src/stories/domain/ChallengePage/CommentButton.stories.tsx
@@ -0,0 +1,10 @@
+import CommentButton from "@domain/ChallengePage/CommentButton";
+
+export default {
+  title: "Component/CommentButton",
+  component: CommentButton,
+};
+
+export const Comment = () => {
+  return <CommentButton count={23} />;
+};
