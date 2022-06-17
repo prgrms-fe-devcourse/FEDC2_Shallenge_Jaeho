41fb26b617d2fd726a4532a55f980c9aa6a2e4b2
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-06-18 00:55:23 +0900

    feat: CertificationButton hover시 반응 추가

--- src/components/domain/ChallengePage/CertificationButton.tsx
+++ src/components/domain/ChallengePage/CertificationButton.tsx
@@ -9,9 +9,16 @@ const CertificationButton = ({ isActive }: activeType) => {
   const bgColor = isActive ? "#FF7900" : "#D9D9D9";
   const name = isActive ? "arrow-up" : "check";
   const color = isActive ? "#FFFFFF" : "#000000";
+  const hoverBgColor = isActive ? "#FFAA6D" : undefined;
+  const cursor = isActive ? "pointer" : undefined;
 
   return (
-    <Circle size="115px" bg={bgColor}>
+    <Circle
+      size="115px"
+      bg={bgColor}
+      cursor={cursor}
+      _hover={{ bg: hoverBgColor }}
+    >
       <Icon name={name} color={color} />
     </Circle>
   );
