014822e0927c3e91eae6a4dab2b3460e3e5b94e3
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-06-17 17:33:55 +0900

    chore: query 객체 변환을 위한 qs 라이브러리 설치

--- src/pages/SearchUserPage.tsx
+++ src/pages/SearchUserPage.tsx
@@ -1,21 +1,89 @@
 import InputLayout from "@layout/InputLayout";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import QueryString from "qs";
 import { Flex, Text } from "@chakra-ui/react";
-const dummyInput = "@김땡땡";
-const dummyUser = [];
+
+const dummyUser = [
+  {
+    coverImage: "자기소개",
+    image: "프로필이미지주소", // 프로필 이미지
+    role: "role",
+    emailVerified: Boolean, // 사용되지 않음
+    banned: Boolean, // 사용되지 않음
+    isOnline: true,
+    posts: [],
+    likes: [],
+    comments: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    messages: [],
+    _id: "userid1", // 사용자 id
+    fullName: "사용자닉네임", //
+    email: "asd@asd.com",
+    createdAt: "2022-05-12T09:44:19.128Z",
+    updatedAt: "2022-05-12T09:44:19.128Z",
+  },
+  {
+    coverImage: "자기소개",
+    image: "프로필이미지주소", // 프로필 이미지
+    role: "role",
+    emailVerified: Boolean, // 사용되지 않음
+    banned: Boolean, // 사용되지 않음
+    isOnline: true,
+    posts: [],
+    likes: [],
+    comments: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    messages: [],
+    _id: "userid1", // 사용자 id
+    fullName: "사용자닉네임", //
+    email: "asd@asd.com",
+    createdAt: "2022-05-12T09:44:19.128Z",
+    updatedAt: "2022-05-12T09:44:19.128Z",
+  },
+  {
+    coverImage: "자기소개",
+    image: "프로필이미지주소", // 프로필 이미지
+    role: "role",
+    emailVerified: Boolean, // 사용되지 않음
+    banned: Boolean, // 사용되지 않음
+    isOnline: true,
+    posts: [],
+    likes: [],
+    comments: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    messages: [],
+    _id: "userid1", // 사용자 id
+    fullName: "사용자닉네임", //
+    email: "asd@asd.com",
+    createdAt: "2022-05-12T09:44:19.128Z",
+    updatedAt: "2022-05-12T09:44:19.128Z",
+  },
+];
 
 const SearchUserPage = () => {
+  const location = useLocation();
+  const query = QueryString.parse(location.search, { ignoreQueryPrefix: true });
+  console.log(query.userName);
+  const [userName, setUserName] = useState(query.userName as string);
+  // userName으로 user을 찾는 로직
   return (
     <InputLayout placeholder="@김땡땡">
       <Flex justifyContent={"center"} width="100%">
         {dummyUser.length === 0 ? (
           <Text fontSize="lg" paddingTop="80px">
-            <span>{dummyInput}</span>으로 찾은 사용자가 없어요 😢
+            <span>{userName}</span>으로 찾은 사용자가 없어요 😢
             <br />
             다른 이름으로 찾아보시는 건 어떠신가요?
           </Text>
         ) : (
-          ""
+          <Flex></Flex>
         )}
       </Flex>
     </InputLayout>
