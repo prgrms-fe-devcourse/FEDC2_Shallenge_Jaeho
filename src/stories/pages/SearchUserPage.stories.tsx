ed4d2d72baa24159f81d55b535723bb0fdf83ecf
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-06-17 12:59:37 +0900

    refactor: emotion 으로 global style 적용

---
+++ src/stories/pages/SearchUserPage.stories.tsx
@@ -0,0 +1,9 @@
+import SearchUserPage from "@pages/SearchUserPage";
+export default {
+  title: "Pages/SearchUserPage",
+  compoenent: "SearchUserPage",
+};
+
+export const Default = () => {
+  return <SearchUserPage></SearchUserPage>;
+};
