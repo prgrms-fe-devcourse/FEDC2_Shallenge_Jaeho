0abbe6984de76ff5c94f86080112979c29477336
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-06-17 14:48:44 +0900

    refactor: layout app에 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import InputLayout from "@layout/InputLayout";
 import HomePage from "@pages/HomePage";
 function App() {
-  return <HomePage></HomePage>;
+  return (
+    <InputLayout placeholder="@김땡땡">
+      <HomePage />
+    </InputLayout>
+  );
 }
 
 export default App;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,28 +7,26 @@ const dummyData = [{}, {}, {}, {}, {}, {}, {}, {}];
 
 const HomePage = () => {
   return (
-    <InputLayout placeholder="@김땡땡">
-      <>
-        <Flex paddingTop="48px" justifyContent="space-between">
-          <Heading size="xl">💪운동</Heading>
-          <Text size="sm" color="#ff7900" alignSelf={"end"}>
-            more{">"}
-          </Text>
-        </Flex>
-        {dummyData.slice(0, 2).map((challange) => {
-          return (
-            <Card
-              type="challange"
-              heading="헬스장에서 30분 이상 운동"
-              text="가지고 싶었던 운동화 구입"
-              commentCount={12}
-              cheerCount={23}
-              margin="16px 0"
-            ></Card>
-          );
-        })}
-      </>
-    </InputLayout>
+    <>
+      <Flex paddingTop="48px" justifyContent="space-between">
+        <Heading size="xl">💪운동</Heading>
+        <Text size="sm" color="#ff7900" alignSelf={"end"}>
+          more{">"}
+        </Text>
+      </Flex>
+      {dummyData.slice(0, 2).map((challange) => {
+        return (
+          <Card
+            type="challange"
+            heading="헬스장에서 30분 이상 운동"
+            text="가지고 싶었던 운동화 구입"
+            commentCount={12}
+            cheerCount={23}
+            margin="16px 0"
+          ></Card>
+        );
+      })}
+    </>
   );
 };
 
