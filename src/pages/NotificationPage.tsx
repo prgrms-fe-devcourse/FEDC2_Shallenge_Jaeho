f64b730cb2cb3d8e43ca00ca7083dde2cd467202
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-06-18 15:51:30 +0900

    feat: NotificationPage 구현

--- src/pages/NotificationPage.tsx
+++ src/pages/NotificationPage.tsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { Notification } from "src/types";
+import NotiComponent from "@domain/NotificationPage/Notification";
+import { Flex } from "@chakra-ui/react";
 
 const NotificationPage = () => {
-  return <div>NotificationPage</div>;
+  const notificationList: Notification[] = [
+    {
+      seen: false,
+      _id: "notificationId1",
+      author: {
+        coverImage: "자기소개",
+        image: "프로필이미지주소", // 프로필 이미지
+        role: "role",
+        emailVerified: true, // 사용되지 않음
+        banned: false, // 사용되지 않음
+        isOnline: true,
+        posts: [],
+        likes: [],
+        comments: [],
+        followers: [],
+        following: [],
+        notifications: [],
+        messages: [],
+        _id: "userid1", // 사용자 id
+        fullName: "사용자닉네임", //
+        email: "asd@asd.com",
+        createdAt: "2022-05-12T09:44:19.128Z",
+        updatedAt: "2022-05-12T09:44:19.128Z",
+      },
+      user: "user1",
+      post: "",
+      follow: "follower1",
+      createdAt: "2022-05-12T09:44:19.128",
+      updatedAt: "2022-05-12T09:44:19.128",
+    },
+  ];
+
+  return (
+    <Flex direction="column">
+      <NotiComponent
+        alarmType="follow"
+        avatarSrc=""
+        userName="사용자1"
+      ></NotiComponent>
+      {notificationList.map((notification) => {
+        return (
+          <NotiComponent
+            alarmType={notification.follow ? "follow" : "comment"}
+            avatarSrc=""
+            userName="사용자1"
+          ></NotiComponent>
+        );
+      })}
+    </Flex>
+  );
 };
 
 export default NotificationPage;
--- src/types/index.ts
+++ src/types/index.ts
@@ -85,3 +85,16 @@ export interface User {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface Notification {
+  seen: boolean;
+  _id: string;
+  author: User;
+  user: User | string;
+  post: string | null; // 포스트 id
+  follow?: string; // 사용자 id
+  comment?: Comment;
+  message?: string; // 메시지 id
+  createdAt: string;
+  updatedAt: string;
+}
