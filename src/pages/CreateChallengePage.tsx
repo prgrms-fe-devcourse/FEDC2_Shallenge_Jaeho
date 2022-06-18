16f0abd515ba5902d3f6ec8a9cfe60c3514eccc7
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-06-18 20:50:18 +0900

    feat: CreateChallengePage UI 완성

--- src/pages/CreateChallengePage.tsx
+++ src/pages/CreateChallengePage.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import ChakraInput from "@base/ChakraInput";
+import { Button, Flex, Heading } from "@chakra-ui/react";
+import ChallengeChannelRadio from "@domain/CreateChallengePage/ChallengeChannelRadio";
+import TextLayout from "@layout/TextLayout";
 
 const CreateChallengePage = () => {
-  return <div>CreateChallengePage</div>;
+  return (
+    <TextLayout text="30일 챌린지 만들기">
+      <Flex
+        margin="40px 0px"
+        direction="column"
+        gap="56px"
+        padding="40px"
+        w="100%"
+        bg="#FFFFFF"
+        borderRadius="15px"
+      >
+        <Flex direction="column" gap="16px">
+          <Heading size="xl">🎯 목표</Heading>
+          <ChakraInput
+            placeholder="목표를 작성해주세요 \(@^0^@)/"
+            variant="outline"
+            width="100%"
+          ></ChakraInput>
+        </Flex>
+        <Flex direction="column" gap="16px">
+          <Heading size="xl">🗂 채널</Heading>
+          <ChallengeChannelRadio />
+        </Flex>
+        <Flex direction="column" gap="16px">
+          <Heading size="xl">📆 시작일</Heading>
+          <ChakraInput
+            placeholder="시작일을 선택해주세요 \(*°▽°*)/"
+            variant="outline"
+            width="100%"
+          ></ChakraInput>
+        </Flex>
+        <Flex direction="column" gap="16px">
+          <Heading size="xl">🏁 보상</Heading>
+          <ChakraInput
+            placeholder="평소 원하는 것을 입력해주세요 ( $ _ $ )"
+            variant="outline"
+            width="100%"
+          ></ChakraInput>
+        </Flex>
+        <Flex justifyContent="center">
+          <Button
+            size="lg"
+            bg="#FFAA6D"
+            w="240px"
+            marginTop="20px"
+            color="#FFFFFF"
+            _hover={{ bg: "#FF7900" }}
+          >
+            생성하기
+          </Button>
+        </Flex>
+      </Flex>
+    </TextLayout>
+  );
 };
 
 export default CreateChallengePage;
---
+++ src/stories/pages/CreateChallengePage.stories.tsx
@@ -0,0 +1,15 @@
+import CreateChallengePage from "@pages/CreateChallengePage";
+import GlobalStyles from "src/styles/GlobalStyles";
+export default {
+  title: "Pages/CreateChallengePage",
+  compoenent: "CreateChallengePage",
+};
+
+export const Default = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <CreateChallengePage></CreateChallengePage>
+    </>
+  );
+};
