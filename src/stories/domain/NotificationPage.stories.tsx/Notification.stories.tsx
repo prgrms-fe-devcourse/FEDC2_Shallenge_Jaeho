b3d92b91ca2ee38cd92cdebe7dfbf9184c936027
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-06-18 12:42:22 +0900

    feat: Notification 컴포넌트 만들기

---
+++ src/stories/domain/NotificationPage.stories.tsx/Notification.stories.tsx
@@ -0,0 +1,15 @@
+import Notification from "@domain/NotificationPage/Notification";
+import { Flex } from "@chakra-ui/react";
+export default {
+  title: "Component/Domain/NotificationPage/Notification",
+  component: Notification,
+};
+
+export const Default = () => {
+  return (
+    <Flex w="610px" height="100vh" flexDirection="column">
+      <Notification avatarSrc="" userName="사용자1" alarmType="follow" />
+      <Notification avatarSrc="" userName="사용자1" alarmType="comment" />
+    </Flex>
+  );
+};
