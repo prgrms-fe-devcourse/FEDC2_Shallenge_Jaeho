bcb6c0ddc9b83eb4eecf62ddf860813fbcc8542b
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-06-18 22:18:38 +0900

    feat: ChakraInput 값 변경 prop,event 추가

--- src/components/base/ChakraInput.tsx
+++ src/components/base/ChakraInput.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Input } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 type inputTypes = {
   placeholder: string;
@@ -6,20 +8,34 @@ type inputTypes = {
   size?: string;
   width?: number | string;
   type?: string;
+  children?: string;
+  onChangeValue?: any;
 };
 
 const ChakraInput = ({
+  children = "",
   placeholder = "placeholder입니다",
   variant = "outline",
   size = "lg",
   width = 400,
   type = "text",
+  onChangeValue,
 }: inputTypes) => {
   const bgColor = variant === "outline" ? "#FFFFFF" : "#E2E8F0";
   const border = variant === "outline" ? "2px solid" : "2px solid transparent";
   const borderColor = variant === "outline" ? "#E2E8F0" : "transparent";
+  const [value, setValue] = useState(children);
+
+  useEffect(() => {
+    onChangeValue ? onChangeValue(value) : undefined;
+  }, [value]);
+
   return (
     <Input
+      value={value}
+      onChange={(event) => {
+        setValue(event.target.value);
+      }}
       type={type}
       placeholder={placeholder}
       size={size}
