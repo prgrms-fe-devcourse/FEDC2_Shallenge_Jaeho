a7c15e4df2279f556974c427af55704660f76996
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-06-18 19:26:21 +0900

    feat: ChallengePage prop 모두 제거

--- src/pages/ChallengePage.tsx
+++ src/pages/ChallengePage.tsx
@@ -6,38 +6,53 @@ import CommentButton from "@domain/ChallengePage/CommentButton";
 import CheerUpButton from "@domain/ChallengePage/CheerUpButton";
 import CertificationButton from "@domain/ChallengePage/CertificationButton";
 
-type CertificationBoxType = {
-  day: number;
-  isChecked: boolean;
-};
+const dummyData = [
+  { day: 1, isChecked: true },
+  { day: 2, isChecked: true },
+  { day: 3, isChecked: false },
+  { day: 4, isChecked: true },
+  { day: 5, isChecked: false },
+  { day: 6, isChecked: false },
+  { day: 7, isChecked: true },
+  { day: 8, isChecked: true },
+  { day: 9, isChecked: false },
+  { day: 10, isChecked: false },
+  { day: 11, isChecked: true },
+  { day: 12, isChecked: true },
+  { day: 13, isChecked: true },
+  { day: 14, isChecked: false },
+  { day: 15, isChecked: true },
+  { day: 16, isChecked: true },
+  { day: 17, isChecked: true },
+  { day: 18, isChecked: true },
+  { day: 19, isChecked: true },
+  { day: 20, isChecked: false },
+  { day: 21, isChecked: false },
+  { day: 22, isChecked: true },
+  { day: 23, isChecked: false },
+  { day: 24, isChecked: true },
+  { day: 25, isChecked: false },
+  { day: 26, isChecked: false },
+  { day: 27, isChecked: true },
+  { day: 28, isChecked: true },
+  { day: 29, isChecked: false },
+  { day: 30, isChecked: false },
+];
 
-type ChallengePageType = {
-  isGuest?: boolean;
-  title?: string;
-  startDate?: string;
-  endDate?: string;
-  reward?: string;
-  restDay?: number;
-  days?: Array<CertificationBoxType> | undefined;
-  commentCount?: number | undefined;
-  CheerUpCount?: number | undefined;
-  isActive?: boolean;
-};
-
-const ChallengePage = ({
-  isGuest = true,
-  title = "Challenge 제목",
-  startDate = "2022.05.23",
-  endDate = "2022.06.23",
-  reward = "보상 내용",
-  restDay = 30,
-  days,
-  commentCount = 0,
-  CheerUpCount = 0,
-  isActive = true,
-}: ChallengePageType) => {
+const ChallengePage = () => {
+  const ChallengeTitle = "Challenge 제목"; // title.challengeTitle
+  const reward = "보상 내용"; // title.reward
+  const commentCount = 0; // comments.length
+  const cheerUpCount = 0; // likes.length
+  const days = dummyData; // title.days
+  const startDate = "2022.05.23"; // title.startDate
+  const presentDay = 0; // (계산) 현재날짜 - 시작일, 최소값 0
+  const restDay = 30 - presentDay; // (계산) 30 - presentDay, 최대값 30
+  const endDate = "2022.06.23"; // (계산) 시작일 + 30
+  const isActive = true; // days[presentDay].isChecked
+  const isGuest = true; // _id 값과 비교할 현재 유저 _id
   return (
-    <TextLayout text={title}>
+    <TextLayout text={ChallengeTitle}>
       <Flex marginTop="16px">
         <ChallengeReward
           startDate={startDate}
@@ -60,10 +75,10 @@ const ChallengePage = ({
         </Flex>
         <Flex gap="160px" marginBottom="26px">
           <CommentButton count={commentCount}></CommentButton>
-          <CheerUpButton count={CheerUpCount}></CheerUpButton>
+          <CheerUpButton count={cheerUpCount}></CheerUpButton>
         </Flex>
       </Flex>
-      {isGuest ? undefined : (
+      {!isGuest && (
         <Circle
           position="absolute"
           left="50%"
