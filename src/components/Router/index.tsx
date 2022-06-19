88357570d19cb636f061b4d7103a7d79a8552a25
sa02045 <sa02045@naver.com>
2022-06-19 21:48:37 +0900

    fix: PrivateRoute 추가 및 AuthProvider 추가

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,4 +1,4 @@
-import AuthPage from "@pages/AuthPage";
+import LoginPage from "@pages/LoginPage";
 import ChallengePage from "@pages/ChallengePage";
 import ChallengesPage from "@pages/ChallengesPage";
 import CommentPage from "@pages/CommentPage";
@@ -11,29 +11,76 @@ import NotificationPage from "@pages/NotificationPage";
 import SearchUserPage from "@pages/SearchUserPage";
 import UserProfilePage from "@pages/UserProfilePage";
 import { Route, Routes } from "react-router-dom";
+import MyProfilePage from "@pages/MyProfilePage";
+
+import AuthProvider from "../Provider/AuthProvider";
+import PrivateRoute from "./PrivateRoute";
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route
-        path="/challenges/:channelId/:challengeId"
-        element={<ChallengePage />}
-      />
-      <Route path="/challenges/:channelId" element={<ChallengesPage />} />
-      <Route
-        path="/challenges/:channelId/:challengeId/comment"
-        element={<CommentPage />}
-      />
-      <Route path="/challenges/create" element={<CreateChallengePage />} />
-      <Route path="/profile/:userId/edit" element={<EditProfilePage />} />
-      <Route path="/follow/:userId" element={<FollowPage />} />
-      <Route path="/notification/:userId" element={<NotificationPage />} />
-      <Route path="/search" element={<SearchUserPage />} />
-      <Route path="/profile/:userId" element={<UserProfilePage />} />
-      <Route path="/*" element={<NotFoundPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route
+          path="/my/profile/"
+          element={
+            <PrivateRoute>
+              <MyProfilePage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/my/profile/edit"
+          element={
+            <PrivateRoute>
+              <EditProfilePage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/my/challenge/create"
+          element={
+            <PrivateRoute>
+              <CreateChallengePage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/my/notifications"
+          element={
+            <PrivateRoute>
+              <NotificationPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="/follow/:userId" element={<FollowPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route
+          path="/challenges/:channelId/:challengeId"
+          element={<ChallengePage />}
+        />
+
+        <Route path="/challenges/:channelId" element={<ChallengesPage />} />
+
+        <Route
+          path="/challenges/:channelId/:challengeId/comment"
+          element={<CommentPage />}
+        />
+
+        <Route path="/search" element={<SearchUserPage />} />
+
+        <Route path="/profile/:userId" element={<UserProfilePage />} />
+
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </AuthProvider>
   );
 };
 
