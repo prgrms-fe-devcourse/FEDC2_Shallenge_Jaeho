bc537dc971354aa8a8ce1310ce60d0923711737f
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-06-19 21:31:05 +0900

    feat: InputLayout submit event 추가

--- src/layout/InputLayout.tsx
+++ src/layout/InputLayout.tsx
@@ -1,22 +1,34 @@
+import { useNavigate } from "react-router-dom";
 import Header from "@layout/Header";
 import Footer from "@layout/Footer";
 import ChakraInput from "@base/ChakraInput";
 import { Box, Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 type InputLayoutTypes = {
   children: React.ReactNode;
   placeholder: string;
 };
 const InputLayout = ({ children, placeholder }: InputLayoutTypes) => {
+  const navigate = useNavigate();
+
+  const [inputText, setInputText] = useState("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    navigate(`/search?userName=${inputText}`);
+  };
   return (
     <Flex justifyContent="center">
       <Flex flexDirection="column" width="640px" alignItems="center">
         <Flex position="sticky" width="100%">
           <Header>
-            <ChakraInput
-              placeholder={placeholder}
-              variant="filled"
-            ></ChakraInput>
+            <form onSubmit={handleSubmit}>
+              <ChakraInput
+                placeholder={placeholder}
+                variant="filled"
+                onChangeValue={setInputText}
+              ></ChakraInput>
+            </form>
           </Header>
         </Flex>
         <Box
