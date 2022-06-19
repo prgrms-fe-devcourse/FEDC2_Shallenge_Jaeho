bb1e00d1bbd1a5735185f1240cbf13585e051a85
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-06-19 18:06:27 +0900

    feat: Comment 컴포넌트 prop 생성

--- src/components/domain/CommentPage/Comment.tsx
+++ src/components/domain/CommentPage/Comment.tsx
@@ -1,14 +1,13 @@
 import Icon from "@base/Icon";
 import { Avatar, Flex, Heading, Text } from "@chakra-ui/react";
 
-// API 추가시 comment 객체 prop으로 받도록 작성하면 완성
-const Comment = () => {
+const Comment = ({
+  avatarSrc = undefined,
+  commentAuthor = "author.fullName",
+  commentContent = "Lorem ipsum dolor sit, amet consectetur",
+  commentCreatedAt = "2022-06-23 17:23",
+}) => {
   const avatarSize = "md";
-  const avatarSrc = undefined; // API추가시 author.image
-  const commentAuthor = "author.fullName";
-  const commentContent =
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse tempore dolore quisquam iure asperiores, expedita eum soluta aliquid voluptatum quia natus ut laborum, aspernatur quos! Quaerat ipsum commodi nesciunt dolore.";
-  const commentCreated = "2022-06-23"; // createdAt
   return (
     <Flex
       padding="15px"
@@ -23,7 +22,7 @@ const Comment = () => {
           <Heading size="sm">{commentAuthor}</Heading>
           <Flex alignItems="flex-end" gap="5px">
             <Text fontSize="md" color="#838489">
-              {commentCreated}
+              {commentCreatedAt}
             </Text>
             <Icon name="trash-2" size={16} color="#FF5A5A"></Icon>
           </Flex>
