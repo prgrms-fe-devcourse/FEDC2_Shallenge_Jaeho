d5d3405a27584c018d08c2077691fa73ca3c8ca9
sa02045 <sa02045@naver.com>
2022-06-21 01:37:01 +0900

    fix: App 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import InputLayout from "@layout/InputLayout";
 import AppRouter from "./components/Router";
+
 function App() {
-  return (
-    <div>
-      <InputLayout placeholder="@김땡땡">
-        <AppRouter />
-      </InputLayout>
-    </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
