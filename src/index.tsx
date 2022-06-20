ef5e6f296712b8e7f7caa78ec4832bdeaf662de9
sa02045 <sa02045@naver.com>
2022-06-21 02:48:51 +0900

    fix: 코드정리

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import GlobalStyles from "@styles/GlobalStyles";
+import App from "./App";
 
 const queryClient = new QueryClient();
 
