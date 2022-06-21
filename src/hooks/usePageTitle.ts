8cc342219e331bf05c3e97e50771cffc7120f2e6
sa02045 <sa02045@naver.com>
2022-06-21 22:34:34 +0900

    fix: bad setState 에러메시지 해결

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -1,9 +1,12 @@
+import { useEffect } from "react";
 import { useAtom } from "jotai";
 import pageTitleAtom from "@store/pageTitle";
 
 const usePageTitle = (title: string) => {
   const [, setPageTitleAtom] = useAtom(pageTitleAtom);
-  setPageTitleAtom(title);
+  useEffect(() => {
+    setPageTitleAtom(title);
+  }, []);
 };
 
 export default usePageTitle;
