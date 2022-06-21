adec7670d737ab6c19f5134eab66237f4cb66ba1
sa02045 <sa02045@naver.com>
2022-06-22 03:38:32 +0900

    feat: 프로필 이미지 업로드 api 추가

--- src/api/user.ts
+++ src/api/user.ts
@@ -6,3 +6,6 @@ export const fetchGetOnlineUserList = () => axios.get("/users/online-users");
 
 export const fetchGetUserById = (userId: string) =>
   axios.get(`/users/${userId}`);
+
+export const fetchPostUserProfileImage = (formData: FormData) =>
+  axios.post("/users/upload-photo", formData);
