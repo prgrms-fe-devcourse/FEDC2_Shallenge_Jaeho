6d69fb8accef17fa6a27b099165d36330891ecab
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-06-30 18:06:32 +0900

    refactor : Card 컴포넌트 반응형 구현

---
+++ src/components/base/Card/ChallangeCard.tsx
@@ -0,0 +1,5 @@
+const ChallangeCard = () => {
+  return <div></div>;
+};
+
+export default ChallangeCard;
---
+++ src/components/base/Card/UserCard.tsx
@@ -0,0 +1,5 @@
+const UserCard = () => {
+  return <div></div>;
+};
+
+export default UserCard;
