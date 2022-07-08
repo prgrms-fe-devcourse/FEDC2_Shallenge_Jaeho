bfb681f6af4252642e85772db5ab3cf33aea1042
wogha95 <wogha95@naver.com>
2022-07-09 06:23:47 +0900

    fix: storybook CommentCard 이름 변경 및 prop적용

--- src/stories/domain/CommentPage/Comment.stories.tsx
+++ src/stories/domain/CommentPage/Comment.stories.tsx
@@ -1,16 +1,23 @@
-import Comment from "@domain/CommentPage/Comment";
+import CommentCard from "@domain/CommentPage/CommentCard";
 import GlobalStyles from "src/styles/GlobalStyles";
 
 export default {
-  title: "Domain/CommentPage/Comment",
-  component: Comment,
+  title: "Domain/CommentPage/CommentCard",
+  component: CommentCard,
 };
 
 export const Default = () => {
   return (
     <>
       <GlobalStyles />
-      <Comment></Comment>
+      <CommentCard
+        isGuest={true}
+        avatarSrc={undefined}
+        commentAuthor={"commentAuthor"}
+        commentAuthorId={"example"}
+        commentContent={"storybook comment"}
+        commentCreatedAt={"2022.06.23 05:23"}
+      ></CommentCard>
     </>
   );
 };
